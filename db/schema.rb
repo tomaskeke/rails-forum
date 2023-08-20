bfb9a1a487633d2ee30cc7d3474f2a8570e5dc79
tomaskeke <tonken.ek@gmail.com>
2023-08-20 15:35:33 +0200

    added users scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_20_125211) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_20_133303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
